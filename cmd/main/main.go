94c8aa75bc75615b1c8ff45612b9faa7ca05fc10
DanilaAnikin <danakin10502@gmail.com>
2024-11-22 16:22:02 +0100

    .

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,17 @@
 package main
 
-import(
-	"fmt"
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/DanilaAnikin/go-bookstore/pkg/routes"
 )
 
 func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
 	
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
